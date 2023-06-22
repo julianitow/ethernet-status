247e55c493404770bbf5519c4974d8ad16ded80f
Julien Guillan <jguillan@medissimo.fr>
2023-06-22 14:34:59 +0200

    Initial Commit

---
+++ ethernet-status/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ethernet-status
+//
+//  Created by Julien Guillan on 22/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ethernet-status/ethernet_statusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ethernet_statusApp.swift
+//  ethernet-status
+//
+//  Created by Julien Guillan on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ethernet_statusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
